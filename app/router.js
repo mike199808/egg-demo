9374e29b5e28e4415b3c43a4d9a55afc58aeba9b
STU-Mike <39616226+STU-Mike@users.noreply.github.com>
2019-03-06 20:41:39 +0800

    Update router.js

--- app/router.js
+++ app/router.js
@@ -6,9 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  // router.get('/test', controller.test.index);
-  // router.get('/sequelize', controller.sequelize.echo);
-  // router.get('/sequelize/createtable', controller.sequelize.createtable);
   router.get('/sequelize/insertUsers', controller.sequelize.insertUsers);
   router.get('/sequelize/insertInformation', controller.sequelize.insertInformation);
   router.get('/sequelize/query', controller.sequelize.query);
